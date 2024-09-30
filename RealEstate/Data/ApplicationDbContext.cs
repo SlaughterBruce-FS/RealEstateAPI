8650f38fa59469fbfe7400e9ffbd3825d314ef1e
SlaughterBruce-FS <BLSlaughter1@student.fullsail.edu>
2024-09-30 10:47:24 -0400

    create seed for properties and added yo databsae

--- RealEstate/Data/ApplicationDbContext.cs
+++ RealEstate/Data/ApplicationDbContext.cs
@@ -14,5 +14,55 @@ namespace RealEstate.Data
         public DbSet<Properties> Properties { get; set; }
 
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Properties>().HasData(
+                new Properties
+                {
+                    Id = 1,
+                    Title = "House for sale",
+                    Description = "A beautiful house for sale",
+                    Price = 100000,
+                    Address = "1234 Main St",
+                    City = "San Francisco",
+                    State = "CA",
+                    Zip = "94123",
+                    Year_Built = "2020",
+                    Bedrooms = 3,
+                    Bathrooms = 2,
+                    Prop_Type = "House",
+                    Prop_Status = "For Sale",
+                    Lot_Size = 2000,
+                    Views = 0,
+                    Slug = "house-for-sale",
+                    Agent_Id = "8720600a-c30a-4502-a0d7-ac38dd9022a2",
+                    Featured_Image= "https://flawlessrealestate.blob.core.windows.net/realestate/1709011290_pexels-expect-best-323780.jpg"
+                },
+                 new Properties
+                 {
+                     Id = 2,
+                     Title = "House for sale",
+                     Description = "A beautiful house for sale",
+                     Price = 100000,
+                     Address = "1234 Main St",
+                     City = "San Francisco",
+                     State = "CA",
+                     Zip = "94123",
+                     Year_Built = "2020",
+                     Bedrooms = 3,
+                     Bathrooms = 2,
+                     Prop_Type = "House",
+                     Prop_Status = "For Sale",
+                     Lot_Size = 2000,
+                     Views = 0,
+                     Slug = "house-for-sale",
+                     Agent_Id = "8720600a-c30a-4502-a0d7-ac38dd9022a2",
+                     Featured_Image = "https://flawlessrealestate.blob.core.windows.net/realestate/1709042821_pexels-jess-loiterton-5007356.jpg"
+                 }
+                );
+        }
+
+
     }
 }
---
+++ RealEstate/Migrations/20240930144604_seedproperties.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RealEstate.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedproperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Properties",
+                columns: new[] { "Id", "Address", "Agent_Id", "Area", "Bathrooms", "Bedrooms", "City", "Date_listed", "Description", "Featured_Image", "Garages", "Is_Published", "Is_Rent", "Lot_Size", "Price", "Prop_Status", "Prop_Type", "Slug", "State", "Title", "Views", "Year_Built", "Zip" },
+                values: new object[,]
+                {
+                    { 1, "1234 Main St", "8720600a-c30a-4502-a0d7-ac38dd9022a2", null, 2, 3, "San Francisco", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "A beautiful house for sale", "https://flawlessrealestate.blob.core.windows.net/realestate/1709011290_pexels-expect-best-323780.jpg", null, null, null, 2000, 100000.0, "For Sale", "House", "house-for-sale", "CA", "House for sale", 0, "2020", "94123" },
+                    { 2, "1234 Main St", "8720600a-c30a-4502-a0d7-ac38dd9022a2", null, 2, 3, "San Francisco", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "A beautiful house for sale", "https://flawlessrealestate.blob.core.windows.net/realestate/1709042821_pexels-jess-loiterton-5007356.jpg", null, null, null, 2000, 100000.0, "For Sale", "House", "house-for-sale", "CA", "House for sale", 0, "2020", "94123" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Properties",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Properties",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
