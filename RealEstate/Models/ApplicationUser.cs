2c3822fa00fd774d210d4358098c2266d1ab6d52
SlaughterBruce-FS <BLSlaughter1@student.fullsail.edu>
2024-09-30 00:19:19 -0400

    update user

---
+++ RealEstate/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RealEstate.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+    }
+}
