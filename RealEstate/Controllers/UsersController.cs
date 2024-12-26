3a0c386be40f1a48ceecf9aa5ad9a5d7f1b0ce12
SlaughterBruce-FS <BLSlaughter1@student.fullsail.edu>
2024-12-26 12:50:55 -0500

    completed userprofiles api

--- RealEstate/Controllers/UsersController.cs
+++ RealEstate/Controllers/UsersController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RealEstate.Data;
 using RealEstate.Models;
 using RealEstate.Models.Dto;
+using RealEstate.Services;
+using RealEstate.Utility;
 using System.Net;
 
 namespace RealEstate.Controllers
@@ -14,11 +17,13 @@ namespace RealEstate.Controllers
 
         private readonly ApplicationDbContext _db;
         private ApiResponse _response;
+        private readonly IBlobService _blobService;
 
-        public UsersController(ApplicationDbContext db)
+        public UsersController(ApplicationDbContext db, IBlobService blobService)
         {
             _db = db;
             _response = new ApiResponse();
+            _blobService = blobService;
         }
 
         [HttpGet("{userId}")]
@@ -82,7 +87,85 @@ namespace RealEstate.Controllers
         }
 
 
-    
+        [HttpPut("{userId}")]
+        public async Task<ActionResult<ApiResponse>> UpdateProfilety(string userId, [FromForm] UpdateUserPorfileDto updateUserPorfileDto)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //if (updateUserPorfileDto == null || userId != null)
+                    //{
+                    //    _response.StatusCode = HttpStatusCode.BadRequest;
+                    //    _response.IsSuccess = false;
+                    //    return BadRequest();
+                    //}
+
+                    UserProfiles userFromdb = await _db.UserProfiles.FindAsync(userId);
+
+
+                
+                    if (userFromdb == null)
+                    {
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.IsSuccess = false;
+                        return BadRequest();
+                    }
+
+                    userFromdb.Phone = updateUserPorfileDto.Phone;
+                    userFromdb.Email = updateUserPorfileDto.Email;
+                    userFromdb.FirstName = updateUserPorfileDto.FirstName;
+                    userFromdb.LastName = updateUserPorfileDto.LastName;
+
+                    if (updateUserPorfileDto.ProfileImage != null && updateUserPorfileDto.ProfileImage.Length > 0 && userFromdb.Profile_Image != null)
+                    {
+
+
+                        string fileName = $"{Guid.NewGuid()}{Path.GetExtension(updateUserPorfileDto.ProfileImage.FileName)}";
+                        await _blobService.DeleteBlob(userFromdb.Profile_Image.Split('/').Last(), SD.SD_STORAGE_CONTAINER);
+
+                        userFromdb.Profile_Image = await _blobService.UploadBlob(fileName, SD.SD_STORAGE_CONTAINER, updateUserPorfileDto.ProfileImage);
+                    }
+
+
+                    if (userFromdb.Profile_Image == null && updateUserPorfileDto.ProfileImage != null)
+                    {
+                        string fileName = $"{Guid.NewGuid()}{Path.GetExtension(updateUserPorfileDto.ProfileImage.FileName)}";
+
+                        userFromdb.Profile_Image = await _blobService.UploadBlob(fileName, SD.SD_STORAGE_CONTAINER, updateUserPorfileDto.ProfileImage);
+
+                    }
+
+                 
+
+
+
+                    _db.UserProfiles.Update(userFromdb);
+                    _db.SaveChanges();
+                    _response.Result = userFromdb;
+                    _response.StatusCode = HttpStatusCode.NoContent;
+                    return Ok(_response);
+
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+                return BadRequest(_response);
+            }
+
+            return _response;
+        }
+
+
     }
+
+
     
 }
---
+++ RealEstate/Models/Dto/UpdateUserPorfileDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RealEstate.Models.Dto
+{
+    public class UpdateUserPorfileDto
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public IFormFile? ProfileImage { get; set; }
+
+    }
+}
